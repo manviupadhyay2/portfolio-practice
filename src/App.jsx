5badea0fe7745bf9d1f040dc4f3ce8a2ada98b1a
Manvi <manviupadhyay2@gmail.com>
2024-10-02 18:10:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+const App = () => {
+  return (
+    <div>
+    <h1 className="text-3xl underline font-bold">
+    Hello world!
+  </h1>
+    </div>
+  )
+}
+
+export default App
