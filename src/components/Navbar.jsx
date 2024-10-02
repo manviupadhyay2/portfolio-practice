a32d0b9af9351ad283b6e0eaa796744328eee9af
Manvi <manviupadhyay2@gmail.com>
2024-10-02 18:36:29 +0530

    modified navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,7 @@
 const Navbar = () => {
   return (
       <nav className="mb-20 flex items-center justify-between py-6">
-          <div className="flex flex-shrink-0 items-center">
-            <img src="" alt="" />
-          </div>
-      
+        NAVBAR
       </nav>
   )
 }
