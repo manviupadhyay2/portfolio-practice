d20618708f0b80a76c672f6a81bef771d63b2870
Manvi <manviupadhyay2@gmail.com>
2024-10-05 22:42:02 +0530

    changed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 const App = () => {
   return (
-    <div>
-       <Navbar/>
+    <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-purple-200 selection:text-black'>
+      <div className='flixed top-0 -z-10 h-full w-full'></div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+
+      <div className='container mx-auto px-8'>
+        <Navbar />
+        <Hero/>
+      </div>
     </div>
   )
 }
