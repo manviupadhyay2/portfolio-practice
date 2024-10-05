fe40781196393bea49cb4b16437e8fdd12588397
Manvi <manviupadhyay2@gmail.com>
2024-10-05 23:57:16 +0530

    done with basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
+import About from './components/About';
+import Experience from './components/Experience';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import Technologies from './components/Technologies';
+
 const App = () => {
   return (
-    <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-purple-200 selection:text-black'>
-      <div className='flixed top-0 -z-10 h-full w-full'></div>
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+    <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-purple-200 selection:text-black relative'>
+      {/* Fullscreen background */}
+      <div className='fixed top-0 left-0 w-full h-full -z-10'>
+        {/* Radial gradient background */}
+        <div className="w-full h-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      </div>
 
-      <div className='container mx-auto px-8'>
+      <div className='container mx-auto px-8 relative'>
         <Navbar />
-        <Hero/>
+        <Hero />
+        <About />
+        <Technologies />
+        <Experience />
+        <Projects/>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import { HERO_CONTENT } from "../constants"
+import profilePic from "../assets/kevinRushProfile.png"
 const Hero = () => {
   return (
       <div className="border-b border-neutral-900 pb-4 lg:mb-35">
@@ -5,7 +7,17 @@ const Hero = () => {
               <div className="w-full lg:w-1/2">
                   <div className="flex flex-col items-center lg:items-start">
                       <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16">Manvi Upadhyay</h1>
+                      <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
+                      Full Stack Developer
+                      </span>
+                      <p>{ HERO_CONTENT }</p>
                   </div>
+              </div>
+              <div className="w-full lg:w-1/2 lg:p-8">
+                  <div className="flex justify-center">
+                    <img src={profilePic} alt="" />
+                  </div>
+              
               </div>
           </div>
       </div>
